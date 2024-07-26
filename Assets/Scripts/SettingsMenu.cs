e9251cceb6aa4b274f97f614a9cf0b162de75853
Jonas <jonas.fonta@gmail.com>
2024-07-26 11:42:20 +0200

    Add initial scene

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using System.Linq;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionsDropdown;
+
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width,  height = resolution.height }).Distinct().ToArray ();
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "*" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+
+        resolutionsDropdown.value = currentResolutionIndex;
+
+        resolutionsDropdown.RefreshShownValue();
+
+        Screen.fullScreen = true;
+
+    }
+
+    public void Setvolume(float volume)
+    {
+        
+        audioMixer.SetFloat("Volume",volume);
+
+
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+    
+    
+    }
+
+}
